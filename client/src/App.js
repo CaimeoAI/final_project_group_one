e74cbdbdd445f74b285888d8efa3721570541569
eleeyeah <ilietmks@gmail.com>
2022-12-09 21:30:00 +0100

    sidebar in viewport sorted

--- client/src/App.js
+++ client/src/App.js
@@ -6,25 +6,29 @@ import LearningSupport from "./pages/LearningSupport";
 import Calendar from "../src/pages/Calendar.js";
 import RegistrationComponent from "./pages/RegistrationComponent";
 import ChatComponent from './pages/ChatComponent'
-import NavBar from "./components/NavBar";
 import Forum from "./pages/Forum";
 
 const App = () => {
   return (
     <>
+    
+     
+    <div className="width-100 flex ">
       <SidebarNav />
-      {/* <NavBar /> */}
-      <div className="width-100 ">
       
       <Routes>
         <Route path="/register" element={<RegistrationComponent />} />
         <Route path="/calendar" element={<Calendar />} />
         <Route path="/learningsupport" element={<LearningSupport />} />
-        <Route path="/forum" element={<Forum />} />
+        <Route path="/academia" element={<Forum />} />
         <Route path="/chat" element={<ChatComponent/>} />
         <Route path="/settings" element={<LearningSupport />} />
       </Routes>
       </div>
+       
+    
+  
+      
     </>
   );
 };
--- client/src/components/learningComponents/Tabs.jsx
+++ client/src/components/learningComponents/Tabs.jsx
@@ -1,30 +1,71 @@
 import * as React from 'react';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 
-export default function ScrollableTabsButtonAuto() {
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
+export default function BasicTabs() {
   const [value, setValue] = React.useState(0);
 
-  const handleChange = (newValue) => {
+  const handleChange = (event, newValue) => {
     setValue(newValue);
   };
 
   return (
-    <Box className='bg-purple-300 mx-2'>
-      <Tabs
-        value={value}
-        onChange={handleChange}
-        variant="scrollable"
-        scrollButtons="auto"
-        aria-label="scrollable auto tabs example"
-      >
-        <Tab label="Classes" />
-        <Tab label="Resources" />
-        <Tab label="Assignments" />
-        <Tab label="Others...bla" />
-      
-      </Tabs>
+    <Box sx={{ width: '100%' }}>
+      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+          <Tab label="Classes" {...a11yProps(0)} />
+          <Tab label="Resources" {...a11yProps(1)} />
+          <Tab label="Quizzes" {...a11yProps(2)} />
+        </Tabs>
+      </Box>
+      <TabPanel value={value} index={0}>
+       <strong>Classes component here</strong> 
+        <br />
+        Contains an overvirew of all mandatory and optional classes and their respective time and date
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        <strong>
+          Resources component here
+        </strong>
+        <br />
+        Contains all the resources available based on your module
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        <strong>
+          Quizzes component here
+        </strong>
+        <br />
+        Contains all the quizzes available in order to test your level 
+      </TabPanel>
     </Box>
   );
 }
