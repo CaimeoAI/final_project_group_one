cabadd5ab65d1d4bfdbb892916d0658d2b7bc094
AninhaCSK <acsk1982@gmail.com>
2022-12-13 15:36:00 +0100

    add event functionality

--- client/src/components/calendar/FullCalend.js
+++ client/src/components/calendar/FullCalend.js
@@ -6,6 +6,33 @@ import interactionPlugin from "@fullcalendar/interaction";
 import listPlugin from "@fullcalendar/list";
 
 export default function FullCalend() {
+  const [currentEvents, setCurrentEvents] = React.useState([]);
+
+  const handleDateClick = (selected) => {
+    const title = prompt("Please enter a new title for your event");
+    const calendarApi = selected.view.calendar;
+    calendarApi.unselect();
+
+    if (title) {
+      calendarApi.addEvent({
+        id: `${selected.dateStr}-${title}`,
+        title,
+        start: selected.startStr,
+        end: selected.endStr,
+        allDay: selected.allDay,
+      });
+    }
+  };
+
+  const handleEventClick = (selected) => {
+    if (
+      window.confirm(
+        `Are you sure you want to delete the event '${selected.event.title}'`
+      )
+    ) {
+      selected.event.remove();
+    }
+  };
   return (
     <div className=" h-[50%] p-2 md:h-[70%] md:w-[80%] md:p-1">
       <FullCalendar
@@ -16,6 +43,26 @@ export default function FullCalend() {
           center: "prev,next",
           right: "dayGridMonth,timeGridWeek,timeGridDay,listMonth",
         }}
+        initialView="dayGridMonth"
+          editable={true}
+          selectable={true}
+          selectMirror={true}
+          dayMaxEvents={true}
+          select={handleDateClick}
+          eventClick={handleEventClick}
+          eventsSet={(events) => setCurrentEvents(events)}
+          initialEvents={[
+            {
+              id: "12315",
+              title: "All-day event",
+              date: "2022-09-14",
+            },
+            {
+              id: "5123",
+              title: "Timed event",
+              date: "2022-09-28",
+            },
+          ]}
       />
     </div>
   );
