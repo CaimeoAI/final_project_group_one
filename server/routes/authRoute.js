0507736725d1e6c605640b3ceea218824b9627a4
edbottura <ebottura@gmail.com>
2022-12-19 10:16:55 -0300

    forgot and reset controllers

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -1,18 +1,24 @@
 //! CORE IMPORTS
-import express from 'express'
-
+import express from "express";
+import {
+  forgotPassword,
+  login,
+  resetPassword,
+  signup,
+} from "../controllers/authController.js";
 //? CONTROLLER IMPORTS
-import { registerUser, loginUser } from '../controllers/authController.js'
+// import { registerUser, loginUser } from "../controllers/authController.js";
 
 //* ROUTER
-const router = express.Router()
+const router = express.Router();
 
-router
-    .route('/login')
-    .post( loginUser )
+//Edson's Router
+router.route("/signup").post(signup);
+router.route("/login").post(login);
+router.route("/forgotPassword").post(forgotPassword);
+router.route("/resetPassword/:token").patch(resetPassword);
 
-router
-    .route('/register')
-    .post( registerUser )
+// router.route("/login").post(loginUser);
+// router.route("/register").post(registerUser);
 
-export default router
+export default router;
