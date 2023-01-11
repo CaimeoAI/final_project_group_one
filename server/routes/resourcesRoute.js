fae71f540f71486bbabc74d14b266689a787200d
eleeyeah <ilietmks@gmail.com>
2023-01-11 17:53:17 +0100

    resourcesRoute update

--- server/routes/resourcesRoute.js
+++ server/routes/resourcesRoute.js
@@ -3,9 +3,18 @@ import express from "express";
 
 //? CONTROLLER IMPORTS
 import {
-    getListOfResources,
-    getResource,
     createResource,
+    getResource,
+    getListOfResources,
     updateResource,
     deleteResource,
 } from "../controllers/resourcesControllers.js";
+
+//* ROUTER
+const router = express.Router();
+
+router.route("/resources").post(createResource).get(getListOfResources);
+
+router.route("/resources/:id").get(getResource).put(updateResource).delete(deleteResource);
+
+export default router;
